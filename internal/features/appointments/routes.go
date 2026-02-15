9643d308b923d0cf2b54c4f7d47753495aa5b01b
agulto-jermainerazehl <agultorazehl@gmail.com>
2026-02-15 16:03:32 +0800

    Refactor appointments routes for eloquent clarity (#7)

--- internal/features/appointments/routes.go
+++ internal/features/appointments/routes.go
@@ -7,48 +7,43 @@ import (
 )
 
 func RegisterRoutes(rg *gin.RouterGroup, h *Handler) {
-	appointmentRoutes := rg.Group("/appointments")
+	
+	routes := rg.Group("/appointments")
 
-	appointmentRoutes.Use(middleware.AuthMiddleware())
+	routes.Use(middleware.AuthMiddleware())
 
-	appointmentRoutes.POST("", middleware.RoleMiddleware(
-		int(constants.StudentRoleID),
-		int(constants.FrontDeskRoleID),
-	), h.Create)
-
-	appointmentRoutes.GET("/all",
-		middleware.RoleMiddleware(
-			int(constants.CounselorRoleID),
-			int(constants.FrontDeskRoleID),
-		), h.HandleListAppointments)
-
-	appointmentRoutes.GET("", middleware.RoleMiddleware(
-		int(constants.StudentRoleID),
-		int(constants.CounselorRoleID),
-		int(constants.FrontDeskRoleID),
-	), h.GetAppointments)
-
-	appointmentRoutes.GET("/:id", middleware.RoleMiddleware(
-		int(constants.StudentRoleID),
-		int(constants.CounselorRoleID),
-		int(constants.FrontDeskRoleID),
-	), h.HandleGetAppointment)
-
-	appointmentRoutes.GET("/student/:studentID", middleware.RoleMiddleware(
-		int(constants.StudentRoleID),
-		int(constants.CounselorRoleID),
-		int(constants.FrontDeskRoleID),
-	), h.HandleGetAppointment)
-
-	appointmentRoutes.PUT("/:id", middleware.RoleMiddleware(
-		int(constants.StudentRoleID),
-		int(constants.CounselorRoleID),
-		int(constants.FrontDeskRoleID),
-	), h.HandleUpdateStatus)
-
-	appointmentRoutes.GET("/slots", middleware.RoleMiddleware(
+	routes.GET("/slots", middleware.RoleMiddleware(
 		int(constants.StudentRoleID),
 		int(constants.CounselorRoleID),
 		int(constants.FrontDeskRoleID),
 	), h.HandleGetAvailableTimeSlots)
+
+	// List All (Counselor)
+	routes.GET("", middleware.RoleMiddleware(
+		int(constants.CounselorRoleID),
+	), h.HandleListAppointments) 
+
+	// List the Appointments (Student)
+	routes.GET("/me", middleware.RoleMiddleware(
+		int(constants.StudentRoleID),
+	), h.GetAppointments) 
+
+	// Create New
+	routes.POST("", middleware.RoleMiddleware(
+		int(constants.StudentRoleID),
+		int(constants.FrontDeskRoleID),
+	), h.Create)
+
+	// Get Details
+	routes.GET("/:id", middleware.RoleMiddleware(
+		int(constants.StudentRoleID),
+		int(constants.CounselorRoleID),
+		int(constants.FrontDeskRoleID),
+	), h.HandleGetAppointment) 
+
+	// Update Status or Reschedule
+	routes.PATCH("/:id", middleware.RoleMiddleware(
+		int(constants.CounselorRoleID),
+		int(constants.FrontDeskRoleID),
+	), h.HandleUpdateStatus)
 }
