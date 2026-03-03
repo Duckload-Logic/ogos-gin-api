ca77455107ab04232f08b5297cbd45e6fcff0824
olazo-johnalbert <jaolazo4@gmail.com>
2026-03-03 16:21:19 +0800

    fix: replace excuse slip repository with slip repository in bootstrap

--- internal/bootstrap/repositories.go
+++ internal/bootstrap/repositories.go
@@ -2,19 +2,19 @@ package bootstrap
 
 import (
 	"github.com/jmoiron/sqlx"
+	"github.com/olazo-johnalbert/duckload-api/internal/features/analytics"
 	"github.com/olazo-johnalbert/duckload-api/internal/features/appointments"
-	"github.com/olazo-johnalbert/duckload-api/internal/features/excuseslips"
 	"github.com/olazo-johnalbert/duckload-api/internal/features/locations"
+	"github.com/olazo-johnalbert/duckload-api/internal/features/slips"
 	"github.com/olazo-johnalbert/duckload-api/internal/features/students"
 	"github.com/olazo-johnalbert/duckload-api/internal/features/users"
-	"github.com/olazo-johnalbert/duckload-api/internal/features/analytics"
 )
 
 type Repositories struct {
 	UserRepo        *users.Repository
 	StudentRepo     *students.Repository
 	AppointmentRepo *appointments.Repository
-	ExcuseSlipRepo  *excuseslips.Repository
+	SlipRepo        *slips.Repository
 	LocationsRepo   *locations.Repository
 	AnalyticsRepo   *analytics.Repository
 }
@@ -24,9 +24,8 @@ func getRepositories(db *sqlx.DB) *Repositories {
 		UserRepo:        users.NewRepository(db),
 		StudentRepo:     students.NewRepository(db),
 		AppointmentRepo: appointments.NewRepository(db),
-		ExcuseSlipRepo:  excuseslips.NewRepository(db),
+		SlipRepo:        slips.NewRepository(db),
 		LocationsRepo:   locations.NewRepository(db),
 		AnalyticsRepo:   analytics.NewRepository(db),
-		
 	}
 }
