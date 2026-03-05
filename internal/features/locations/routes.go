9c9979e808ea162c9642ba1a71faa5189439a254
olazo-johnalbert <jaolazo4@gmail.com>
2026-03-05 22:33:20 +0800

    feat: add endpoint to retrieve cities by region code

--- internal/features/locations/routes.go
+++ internal/features/locations/routes.go
@@ -7,6 +7,7 @@ func RegisterRoutes(r *gin.RouterGroup, h *Handler) {
 	{
 		routes.GET("/regions", h.HandleGetRegions)
 		routes.GET("/regions/:regionCode/provinces", h.HandleGetProvincesByRegion)
+		routes.GET("/regions/:regionCode/cities", h.HandleGetCitiesByRegion)
 		routes.GET("/provinces/:provinceCode/cities", h.HandleGetCitiesByProvince)
 		routes.GET("/cities/:cityCode/barangays", h.HandleGetBarangaysByCity)
 	}
