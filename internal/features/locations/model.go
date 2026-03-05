eefa09ec81788431cd143187ea833b430501b5c9
olazo-johnalbert <jaolazo4@gmail.com>
2026-03-05 22:33:14 +0800

    feat: update City and Address models to support optional region and province codes

--- internal/features/locations/model.go
+++ internal/features/locations/model.go
@@ -23,6 +23,7 @@ type City struct {
 	Type         *string `db:"type" json:"type,omitempty"`
 	ZipCode      *string `db:"zip_code" json:"zipCode,omitempty"`
 	District     *string `db:"district" json:"district,omitempty"`
+	RegionCode   *string `db:"region_code" json:"regionCode,omitempty"`
 }
 
 type Barangay struct {
@@ -35,7 +36,7 @@ type Barangay struct {
 type Address struct {
 	ID           int       `db:"id" json:"id"`
 	RegionCode   string    `db:"region_code" json:"regionCode"`
-	ProvinceCode string    `db:"province_code" json:"provinceCode"`
+	ProvinceCode *string   `db:"province_code" json:"provinceCode,omitempty"`
 	CityCode     string    `db:"city_code" json:"cityCode"`
 	BarangayCode string    `db:"barangay_code" json:"barangayCode"`
 	StreetDetail *string   `db:"street_detail" json:"streetDetail"`
