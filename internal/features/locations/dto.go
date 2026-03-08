3666ebe7ea0b63894a648ca2d6cdecd791b71558
olazo-johnalbert <jaolazo4@gmail.com>
2026-03-08 09:20:09 +0800

    feat(locations): add ProvinceDTO struct to manage province data

--- internal/features/locations/dto.go
+++ internal/features/locations/dto.go
@@ -1,5 +1,7 @@
 package locations
 
+import "github.com/olazo-johnalbert/duckload-api/internal/core/structs"
+
 type AddressDTO struct {
 	ID           int       `json:"id"`
 	StreetDetail string    `json:"streetDetail"`
@@ -7,4 +9,11 @@ type AddressDTO struct {
 	Province     *Province `json:"province,omitempty"`
 	City         City      `json:"city"`
 	Barangay     Barangay  `json:"barangay"`
+}
+
+type ProvinceDTO struct {
+	ID         int                    `json:"id,omitempty"`
+	Code       structs.NullableString `json:"code,omitempty"`
+	Name       structs.NullableString `json:"name,omitempty"`
+	RegionCode structs.NullableString `json:"regionCode,omitempty"`
 }
