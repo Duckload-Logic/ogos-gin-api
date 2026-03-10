8624c5b83b6ae10094a35035b5f4b268cfe3a988
olazo-johnalbert <jaolazo4@gmail.com>
2026-03-11 03:20:05 +0800

    Add NullableInt64 type with JSON marshaling and unmarshaling support

--- internal/core/structs/null_string.go
+++ internal/core/structs/null_string.go
@@ -6,6 +6,7 @@ import (
 )
 
 type NullableString sql.NullString
+type NullableInt64 sql.NullInt64
 
 func (ns *NullableString) UnmarshalJSON(data []byte) error {
 	if string(data) == "null" {
@@ -37,4 +38,37 @@ func FromSqlNull(ns sql.NullString) NullableString {
 
 func ToSqlNull(ns NullableString) sql.NullString {
 	return sql.NullString{String: ns.String, Valid: ns.Valid}
+}
+
+func (ni *NullableInt64) UnmarshalJSON(data []byte) error {
+	if string(data) == "null" {
+		ni.Valid = false
+		ni.Int64 = 0
+		return nil
+	}
+
+	var i int64
+	if err := json.Unmarshal(data, &i); err != nil {
+		return err
+	}
+
+	ni.Int64 = i
+	ni.Valid = true
+	return nil
+}
+
+func (ni NullableInt64) MarshalJSON() ([]byte, error) {
+	if !ni.Valid {
+		return []byte("null"), nil
+	}
+
+	return json.Marshal(ni.Int64)
+}
+
+func FromSqlNullInt64(ni sql.NullInt64) NullableInt64 {
+	return NullableInt64{Int64: ni.Int64, Valid: ni.Valid}
+}
+
+func ToSqlNullInt64(ni NullableInt64) sql.NullInt64 {
+	return sql.NullInt64{Int64: ni.Int64, Valid: ni.Valid}
 }
