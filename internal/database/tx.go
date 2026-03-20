43ae75a456971b0fdcf02efd9cb9bc64f8c752a6
olazo-johnalbert <jaolazo4@gmail.com>
2026-03-20 19:41:37 +0800

    Add generic NewRunInTransaction function for improved transaction handling

--- internal/database/tx.go
+++ internal/database/tx.go
@@ -23,3 +23,29 @@ func RunInTransaction(
 
 	return tx.Commit()
 }
+
+func NewRunInTransaction[T any](
+	ctx context.Context,
+	db *sqlx.DB,
+	fn func(*sqlx.Tx) (T, error),
+) (T, error) {
+	var result T
+	tx, err := db.BeginTxx(ctx, nil)
+	if err != nil {
+		return result, err
+	}
+
+	// Always defer rollback. If Commit is called, Rollback does nothing.
+	defer tx.Rollback()
+
+	result, err = fn(tx)
+	if err != nil {
+		return result, err
+	}
+
+	if err := tx.Commit(); err != nil {
+		return result, err
+	}
+
+	return result, nil
+}
