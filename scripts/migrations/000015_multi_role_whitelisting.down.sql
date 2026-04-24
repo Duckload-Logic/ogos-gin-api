c329c3eee00b721ada82d2f86b9ad3e2ab0c7f75
Albert <155875419+olazo-johnalbert@users.noreply.github.com>
2026-04-25 03:25:58 +0800

    Chore/fix dummy count (#108)
    * chore(migrations): consolidate whitelisting logic and remove redundant migrations
    
    * fix: revert to 50 students

--- scripts/migrations/000015_multi_role_whitelisting.down.sql
+++
@@ -1,9 +0,0 @@
--- ============================================================================
--- REVERT MULTIPLE ROLES PER WHITELISTED EMAIL
--- ============================================================================
-
--- NOTE: This may fail if there are duplicate emails with different roles.
--- Manual cleanup of duplicates would be required before running this.
-
-ALTER TABLE whitelists DROP PRIMARY KEY;
-ALTER TABLE whitelists ADD PRIMARY KEY (email);
